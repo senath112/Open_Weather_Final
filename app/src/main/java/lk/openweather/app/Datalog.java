bb5e1afe5b79a6ee63dcf5eca2347bdf7e959b5e
senath <senathsethmika@gmail.com>
2022-10-19 22:08:02 +0530

    Bluetooth Update

--- app/src/main/java/lk/openweather/app/Datalog.java
+++ app/src/main/java/lk/openweather/app/Datalog.java
@@ -15,4 +15,36 @@ public class Datalog {
         this.pressure = pressure;
         this.timestamp = timestamp;
     }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
